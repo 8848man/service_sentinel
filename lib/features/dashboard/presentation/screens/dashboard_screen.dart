01b38aae50910533d64c5fb5e1c43594e419fda7
rwkim <rwkim@users.noreply.github.com>
2026-03-13 23:19:59 +0900

    refactor(dashboard): move providers to presentation/providers/

--- lib/features/dashboard/presentation/screens/dashboard_screen.dart
+++ lib/features/dashboard/presentation/screens/dashboard_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:service_sentinel_fe_v2/core/state/project_session_notifier.dart';
-import 'package:service_sentinel_fe_v2/features/dashboard/application/providers/dashboard_provider.dart';
+import 'package:service_sentinel_fe_v2/features/dashboard/presentation/providers/dashboard_provider.dart';
 import 'package:service_sentinel_fe_v2/features/dashboard/domain/entities/dashboard_overview.dart';
 import '../../../../core/extensions/context_extensions.dart';
 import '../../../../core/auth/application/providers/auth_provider.dart';
