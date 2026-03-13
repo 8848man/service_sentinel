17a90fa095b746828e7e6d8a2680542ae50ca779
rwkim <rwkim@users.noreply.github.com>
2026-03-14 00:47:53 +0900

    refactor(features/project): replace deep imports with barrel files in project_repository_impl

--- lib/features/project/data/repositories/project_repository_impl.dart
+++ lib/features/project/data/repositories/project_repository_impl.dart
@@ -1,6 +1,4 @@
-import 'package:service_sentinel_fe_v2/features/project/domain/entities/project_health.dart';
-import 'package:service_sentinel_fe_v2/features/project/infrastructure/data_sources/local_project_data_source_impl.dart';
-
+import 'package:service_sentinel_fe_v2/features/project/domain/public.dart';
 import '../../../../core/data/data_source_mode.dart';
 import '../../../../core/error/result.dart';
 import '../../../../core/error/app_error.dart';
